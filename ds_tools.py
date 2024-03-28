fb42f9b135f0b6e4e3bfb4eafea2e5577686e152
nithinsaibalaji <125888414+nithinsaibalaji@users.noreply.github.com>
2024-03-28 23:43:40 +0530

    Add files via upload

---
+++ ds_tools.py
@@ -0,0 +1,187 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "059ff0e8",
+   "metadata": {},
+   "source": [
+    "# EcoSystem of the Data Science"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "e76a54f4",
+   "metadata": {},
+   "source": [
+    "# INTRODUCTION"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "f6fd5306",
+   "metadata": {},
+   "source": [
+    "# In this we'll learn about Data Science Tools and its ecosystem"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "8787b9b8",
+   "metadata": {},
+   "source": [
+    "Languages of DATA SCIENCE:\n",
+    "   1. Java\n",
+    "   2. Python\n",
+    "   3. R\n",
+    "   4. SQL"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "a55f2caf",
+   "metadata": {},
+   "source": [
+    "Some of the commonly used libraries used by Data Scientists\n",
+    "1. Tensorflow\n",
+    "2. NumPy,\n",
+    "3. SdPy.\n",
+    "4. pmdas.\n",
+    "5. XOtVotlb.\n",
+    "6. Ketas.\n",
+    "7. SdKi-Learn."
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "3db27e10",
+   "metadata": {},
+   "source": [
+    "|DS Tools|\n",
+    "|---|\n",
+    "|Tensor|\n",
+    "|Apache|"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "0bbf3bbf",
+   "metadata": {},
+   "source": [
+    "### Arithmetic Expressions in DS"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "19ae4c0d",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "16"
+      ]
+     },
+     "execution_count": 3,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "(5+3)*2"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "ddae69e6",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "125"
+      ]
+     },
+     "execution_count": 4,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "5**3"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "id": "42f3fa4e",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "6.0"
+      ]
+     },
+     "execution_count": 5,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "#Minutes to hours\n",
+    "360/60"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "17deb870",
+   "metadata": {},
+   "source": [
+    "#_Objectives\n",
+    "* DS\n",
+    "* DS Tools\n",
+    "* Ecosystem"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "2ac88b1a",
+   "metadata": {},
+   "source": [
+    "# Done By\n",
+    "O NITHIN SAI BALAJI"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "e79f5f10",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
